d85e59c6adb160020f752fe5513b88e6273d7f01
DigiPie <evan.it.sg@gmail.com>
2020-04-10 17:55:31 +0800

    Fix chai test structure Structure was incorrect previously

--- server/test/productRoutes.test.js
+++ server/test/productRoutes.test.js
@@ -5,57 +5,69 @@ require("../models/Product");
 const Product = mongoose.model("products");
 
 const chai = require("chai");
-const { assert } = chai;
+const { assert, expect } = chai;
 const { should } = chai.should();
+
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
 const chaiHttp = require("chai-http");
+
 const app = require("../index");
 
 chai.use(chaiHttp);
 describe("products", () => {
-  // Empty database before each test case is executed
-  /*afterEach((done) => {
+  // Empty database before all test cases are executed.
+  before((done) => {
     Product.deleteMany({}, (err) => {
       done();
     });
-  });*/
+  });
+
+  // Empty database after all test cases are executed.
+  after((done) => {
+    Product.deleteMany({}, (err) => {
+      done();
+    });
+  });
 
   // Unit tests //
 
   /**
-   * Tests the GET /api/products route.
-   * Expected to return an empty array since no products have been added yet.
-   * Expected to return a 200 OK status code.
+   * Unit test suite for the GET /api/products route.
    */
-  describe("GET /api/products : Success : No documents in Product", () => {
-    it("Delete all documents in Product collection", (done) => {
-      setTimeout(() => {
-        Product.deleteMany({}, (err) => {
-          done();
-        });
-      }, 4);
-    });
-
-    it("Return an empty array with a 200 OK status code.", (done) => {
-      chai
-        .request(app)
-        .get("/api/products")
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("array");
-          res.body.length.should.be.eql(0);
-          done();
-        })
-        .timeout(500);
+  describe("GET /api/products", () => {
+    /**
+     * Tests the GET /api/products route.
+     * Expected to return an empty array since no products have been added yet.
+     * Expected to return a 200 OK status code.
+     */
+    it("Successfully GET an empty array of 0 products.", (done) => {
+      Product.deleteMany({}, (err) => {
+        chai
+          .request(app)
+          .get("/api/products")
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("array");
+            res.body.length.should.be.eql(0);
+            done();
+          })
+          .timeout(500);
+      });
     });
   });
 
   /**
-   * Tests the POST /api/products route with all Product parameters specified.
-   * Expected to return the added Product retrieved from the Product collection.
-   * Expected to return a 200 OK status code.
+   * Unit test suite for the POST /api/products route.
    */
-  describe("POST /api/products : Success : Required and optional parameters specified", () => {
-    it("Return the added Product with a 200 OK status code.", (done) => {
+  describe("POST /api/products", () => {
+    /**
+     * Tests the POST /api/products route with all Product parameters specified.
+     * Expected to return the added Product retrieved from the Product collection.
+     * Expected to return a 200 OK status code.
+     */
+    it("Successfully return added Product.", (done) => {
       const p_name = "Apple";
       const p_price = 1.0;
       const p_quantity = 1;
@@ -81,15 +93,13 @@ describe("products", () => {
         })
         .timeout(500);
     });
-  });
 
-  /**
-   * Tests the POST /api/products route with all Product parameters missing.
-   * Expected to return a ValidationError.
-   * Expected to return a 422 Unprocessable Entity status code.
-   */
-  describe("POST /api/products : Failure : Required Product parameters not specified", () => {
-    it("Return a 422 Unprocessable Entity status code.", (done) => {
+    /**
+     * Tests the POST /api/products route with all Product parameters missing.
+     * Expected to return a ValidationError.
+     * Expected to return a 422 Unprocessable Entity status code.
+     */
+    it("Fail to add Product and successfully return ValidationError.", (done) => {
       chai
         .request(app)
         .post("/api/products")
@@ -103,63 +113,58 @@ describe("products", () => {
   });
 
   // Integration tests //
+
   /**
-   * Tests the POST /api/products and GET /api/products route.
-   * Expected to return an empty array since no products have been added yet.
-   * Expected to return a 200 OK status code.
+   * Integration test suite for the GET /api/products and POST /apip/products route.
    */
-  describe("POST /api/products & GET /api/products : Success : One document in Product", () => {
-    it("Delete all documents in Product collection", (done) => {
-      setTimeout(() => {
-        Product.deleteMany({}, (err) => {
-          done();
-        });
-      }, 4);
-    });
+  describe("GET /api/products & POST /api/products", () => {
+    /**
+     * Tests the POST /api/products and GET /api/products route.
+     * Expected to return an empty array since no products have been added yet.
+     * Expected to return a 200 OK status code.
+     */
+    it("Successfully GET an array of 1 product added by POST.", (done) => {
+      Product.deleteMany({}, (err) => {
+        const p_name = "Apple";
+        const p_price = 1.0;
+        const p_quantity = 1;
+        const p_isListed = false;
 
-    const p_name = "Apple";
-    const p_price = 1.0;
-    const p_quantity = 1;
-    const p_isListed = false;
+        chai
+          .request(app)
+          .post("/api/products")
+          .send({
+            name: p_name,
+            price: p_price,
+            quantity: p_quantity,
+            isListed: p_isListed,
+          })
+          .end((err, res) => {
+            res.should.have.status(200);
+            const { name, price, quantity, isListed } = res.body;
+            chai.assert.equal(name, p_name);
+            chai.assert.equal(price, p_price);
+            chai.assert.equal(quantity, p_quantity);
+            chai.assert.equal(isListed, p_isListed);
+          })
+          .timeout(500);
 
-    it("Return the added Product with a 200 OK status code.", (done) => {
-      chai
-        .request(app)
-        .post("/api/products")
-        .send({
-          name: p_name,
-          price: p_price,
-          quantity: p_quantity,
-          isListed: p_isListed,
-        })
-        .end((err, res) => {
-          res.should.have.status(200);
-          const { name, price, quantity, isListed } = res.body;
-          chai.assert.equal(name, p_name);
-          chai.assert.equal(price, p_price);
-          chai.assert.equal(quantity, p_quantity);
-          chai.assert.equal(isListed, p_isListed);
-          done();
-        })
-        .timeout(500);
-    });
-
-    it("Return an array containing the added Product with a 200 OK status code.", (done) => {
-      chai
-        .request(app)
-        .get("/api/products")
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("array");
-          res.body.length.should.be.eql(1);
-          const { name, price, quantity, isListed } = res.body[0];
-          chai.assert.equal(name, p_name);
-          chai.assert.equal(price, p_price);
-          chai.assert.equal(quantity, p_quantity);
-          chai.assert.equal(isListed, p_isListed);
-          done();
-        })
-        .timeout(500);
+        chai
+          .request(app)
+          .get("/api/products")
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a("array");
+            res.body.length.should.be.eql(1);
+            const { name, price, quantity, isListed } = res.body[0];
+            chai.assert.equal(name, p_name);
+            chai.assert.equal(price, p_price);
+            chai.assert.equal(quantity, p_quantity);
+            chai.assert.equal(isListed, p_isListed);
+            done();
+          })
+          .timeout(500);
+      });
     });
   });
 });
