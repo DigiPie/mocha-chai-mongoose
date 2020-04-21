05561d261d3984f642e27b1ea95f7d33b7d2d63c
DigiPie <evan.it.sg@gmail.com>
2020-04-21 12:06:02 +0800

    Refactor ProductService.js

--- server/services/ProductService.js
+++ server/services/ProductService.js
@@ -3,29 +3,32 @@ const mongoose = require("mongoose");
 const Product = mongoose.model("products");
 
 class ProductService {
-  // Returns a Promise which finds all documents in the Collection
+  /**
+   * Returns a Promise which finds all documents in the Collection.
+   */
   static findAll() {
     return Product.find();
   }
 
-  // Returns a Promise which saves one document to the Collection
-  static saveOne(name, price, quantity, isListed) {
+  /**
+   * Returns a Promise which saves one document to the Collection.
+   */
+  static saveOne(properties) {
     const date = Date.now();
 
-    const product = new Product({
-      name,
-      price,
-      quantity,
-      isListed,
-      dateCreated: date,
-      dateUpdated: date,
-    });
+    // Only pick out valid properties, discard the rest
+    properties = pick(properties, "name", "price", "quantity", "isListed");
+    properties.dateCreated = date; // Append dateUpdated
+    properties.dateUpdated = date; // Append dateUpdated
 
-    return product.save();
+    return new Product(properties).save(); // Return the saved document
   }
 
-  // Returns a Promise which updates one document in the Collection
+  /**
+   * Returns a Promise which updates one document in the Collection.
+   */
   static findByIdAndUpdate(_id, properties) {
+    // If _id is not supplied, short-circuit and return a ValidationError
     if (!_id) {
       let err = new Error("Missing parameter: '_id'.");
       err.name = "ValidationError";
@@ -35,7 +38,7 @@ class ProductService {
     // Only pick out valid properties, discard the rest
     properties = pick(properties, "name", "price", "quantity", "isListed");
     properties.dateUpdated = Date.now(); // Append dateUpdated
-    
+
     return Product.findByIdAndUpdate(
       _id,
       properties,
@@ -43,8 +46,11 @@ class ProductService {
     );
   }
 
-  // Returns a Promise which deletes one document in the Collection
+  /**
+   * Returns a Promise which deletes one document in the Collection.
+   */
   static findByIdAndRemove(_id) {
+    // If _id is not supplied, short-circuit and return a ValidationError
     if (!_id) {
       let err = new Error("Missing parameter: '_id'.");
       err.name = "ValidationError";
