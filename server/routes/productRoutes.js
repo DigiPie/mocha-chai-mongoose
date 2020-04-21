de180d6fb75b612ace9eed992ee11a9ab1020fee
DigiPie <evan.it.sg@gmail.com>
2020-04-21 12:05:50 +0800

    Refactor productRoutes.js

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,94 +2,100 @@ const { isEqual, pick } = require("lodash");
 const ProductService = require("../services/ProductService");
 
 module.exports = (app) => {
-  // Gets all products
+  /**
+   * Returns all products in the collection.
+   */
   app.get("/api/products", async (req, res, next) => {
     try {
       const products = await ProductService.findAll();
       res.send(products);
     } catch (err) {
-      // Pass on unexpected error
       return next(err);
     }
   });
 
-  // Saves a product
+  /**
+   * Saves a product to the collection.
+   */
   app.post("/api/products", async (req, res, next) => {
-    const { name, price, quantity, isListed } = req.body;
-
     try {
-      const product = await ProductService.saveOne(
-        name,
-        price,
-        quantity,
-        isListed
+      // Only pick out valid properties, discard the rest
+      const properties = pick(
+        req.body,
+        "name",
+        "price",
+        "quantity",
+        "isListed"
       );
+      const product = await ProductService.saveOne(properties);
       res.send(product);
     } catch (err) {
-      // If validation error, return 400 Bad Request
-      if (isEqual(err.name, "ValidationError")) {
-        return res.status(400).send(err);
-      }
-
-      // Pass on unexpected error
       return next(err);
     }
   });
 
-  // Updates a product
+  /**
+   * Updates a product in the collection.
+   * 0 to n properties can be specified for updating,
+   * where n is the max number of properties the Product has
+   * besides dateCreated and dateUpdated.
+   */
   app.put("/api/products", async (req, res, next) => {
-    const { _id } = req.body;
-    // Only pick out valid properties, discard the rest
-    const properties = pick(req.body, "name", "price", "quantity", "isListed");
-
     try {
+      const { _id } = req.body;
+
+      // If _id is not supplied, short-circuit and return a ValidationError
+      if (!_id) {
+        let err = new Error("Missing parameter: '_id'.");
+        err.name = "ValidationError";
+        throw err;
+      }
+
+      // Only pick out valid properties, discard the rest
+      const properties = pick(
+        req.body,
+        "name",
+        "price",
+        "quantity",
+        "isListed"
+      );
       const product = await ProductService.findByIdAndUpdate(_id, properties);
       res.send(product);
     } catch (err) {
-      // If validation error, return 400 Bad Request
-      if (isEqual(err.name, "ValidationError")) {
-        return res.status(400).send(err);
-      }
-
-      // If fields are malformed, return 400 Bad Request
-      if (isEqual(err.name, "CastError")) {
-        return res.status(400).send(err);
-      }
-
-      // Pass on unexpected error
       return next(err);
     }
   });
 
-  // Deletes a product
+  /**
+   * Deletes a product in the collection.
+   */
   app.delete("/api/products", async (req, res, next) => {
-    const { _id } = req.body;
-
     try {
+      const { _id } = req.body;
+
+      // If _id is not supplied, short-circuit and return a ValidationError
+      if (!_id) {
+        let err = new Error("Missing parameter: '_id'.");
+        err.name = "ValidationError";
+        throw err;
+      }
+
       const query = await ProductService.findByIdAndRemove(_id);
       res.send(query);
     } catch (err) {
-      // If validation error, return 400 Bad Request
-      if (isEqual(err.name, "ValidationError")) {
-        return res.status(400).send(err);
-      }
-
-      // If _id is malformed, return 400 Bad Request
-      if (isEqual(err.name, "CastError")) {
-        return res.status(400).send(err);
-      }
-
-      // Pass on unexpected error
-      return next(err);
+      next(err);
     }
   });
 
-  // Catch-all error handling
+  /**
+   * Catch-all error handler for unexpected errors.
+   */
   app.use(function (err, req, res, next) {
-    if (req.xhr) {
-      res.status(500).send(err);
-    } else {
-      next(err);
+    // If validation or cast error, return 400 Bad Request
+    if (isEqual(err.name, "ValidationError") || isEqual(err.name, "CastError")) {
+      res.status(400).send(err);
     }
+
+    res.status(500).send(err);
   });
 };
